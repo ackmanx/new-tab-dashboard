d67c7673d22b38b972c7dd9ae029e283908ecf11
ackmanx <ackmanx@users.noreply.github.com>
2023-05-14 16:08:38 -0500

    Switch to React instead of Svelte

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.css'
+import { AddNewPlaceholder } from './components/AddNewPlaceholder'
 
 function App() {
-  return <>hi</>
+  return (
+    <main>
+      <section>
+        <h2>Pull Requests</h2>
+        <ul>
+          <li>
+            <a href=''>All of Mine</a>
+          </li>
+          <li>
+            <a href=''>Frontend (UI, Prefix, ABP)</a>
+          </li>
+          <li>
+            <a href=''>Slingshot UI</a>
+          </li>
+        </ul>
+      </section>
+
+      <AddNewPlaceholder />
+    </main>
+  )
 }
 
 export default App
---
+++ src/components/AddNewPlaceholder.tsx
@@ -0,0 +1,10 @@
+import AddIcon from './add-icon.png'
+import './AddNewPlaceholder.css'
+
+export const AddNewPlaceholder = () => {
+  return (
+    <section>
+      <img src={AddIcon} alt='add card icon' />
+    </section>
+  )
+}
