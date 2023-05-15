4679d4eea35d34df5e8c31d7fe80ebce1970c440
Eric Majerus <Eric.Majerus@target.com>
2023-05-15 13:09:50 -0500

    Using css variables

--- src/components/AddNewPlaceholder/AddNewPlaceholder.css.ts
+++ src/components/AddNewPlaceholder/AddNewPlaceholder.css.ts
@@ -1,14 +1,14 @@
 import { style } from '@vanilla-extract/css'
 
 const section = style({
-  backgroundColor: 'rgba(255, 255, 255, 0.09)',
+  backgroundColor: 'var(--dark__card-bg)',
   padding: '16px',
   borderRadius: '10px',
   display: 'flex',
   justifyContent: 'center',
   cursor: 'pointer',
 
-  ':hover': { border: '1px solid #ffffffde', boxSizing: 'border-box' },
+  ':hover': { outline: '1px solid var(--dark__text)', boxSizing: 'border-box' },
 })
 
 const icon = style({ width: '50px' })
